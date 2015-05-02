cc5326e4e3f042e55b219e4524394d7b57afb149
Randy Davis <randy_davis@outlook.com>
2015-05-02 15:49:07 -0500

    Functions - Looping: finish looping example to create animal farm

--- js-fundamentals-to-functionaljs/script.js
+++ js-fundamentals-to-functionaljs/script.js
@@ -1,11 +1,28 @@
-// Functions - Constructors
+// Functions - Looping
 
-function AnimalMaker(name) {
+var AnimalMaker = function(name) {
     return {
         speak: function() {
             console.log("my name is ", name);
-        }
+        },
+        name: name,
+        owner: 'Randy'
     };
 };
 
 var animalNames = ['Sheep', 'Liger', 'Big Bird'];
+
+var farm = [];
+
+for(var i = 0; i < animalNames.length; i++) {
+    //var myAnimal = new AnimalMaker(animalNames[i]); // OR
+    var myAnimal= AnimalMaker(animalNames[i])
+    farm.push(myAnimal);
+    // The next line shows the previous 2 lines condensed onto one
+    // farm.push(AnimalMaker(animalNames[i]));
+    console.log(myAnimal.speak());
+}
+
+for(var i =0; i < farm.length; i++) {
+    farm[i].speak();
+}
