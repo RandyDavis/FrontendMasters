ff1009b2a391d5c7a7d678152281273717a29a83
Randy Davis <randy_davis@outlook.com>
2015-05-04 22:56:32 -0500

    Scope - Local and Global Scope: quick examples

--- js-fundamentals-to-functionaljs/script.js
+++ js-fundamentals-to-functionaljs/script.js
@@ -1,80 +1,27 @@
-// Nesting - Exercise
+// Scope - Local and Global Scope
 
-// Previous Exercise Code
-var AnimalTestUser = function(username) {
-    var argsLength = arguments.length;
-    var otherArgs = [];
+// Anatomy of a function
+var add = function(a, b) {
+  return a + b;
+};
 
-    if (argsLength > 1) {
-        for(var i = 1; i < argsLength; i++) {
-            otherArgs.push(arguments[i]);
-        }
-    }
-}
+console.log(add(3, 4, 5)); // 7
 
-var AnimalCreator = function(username, species, tagline, noises) {
-    return {
-        username: username,
-        species: species,
-        tagline: tagline,
-        noises: noises,
-        friends: []
-    }
-}
+// Local scope 
+var func = function() {
+  var local = true;
+};
 
-var addFriend = function(animal, friend) {
-    animal.friends.push(friend.username);
-}
+//console.log(local); // undefined
 
 
-var sheep = AnimalCreator('Cloudy', 'sheep', 'Baaaah Fool', ['Baaaah', 'Baaaaaaaaaaah', 'baahbaah']);
-var cow = AnimalCreator('Bessy', 'cow', 'Moooo Fool', ['Mooooo', 'Mooooooooove', 'moomoo']);
-var llama = AnimalCreator('Edward', 'llama', 'Llama Sound Fool', ['Llama Sound', 'Coffee?', 'Perhaps Tea?']);
+// Global scope
+var x = 'global';
+ // inside a function
+ function encapsulate() {
+  z = "global here, too!"; // leaving "var" off automatically makes variable part of global scope
 
-var myFarm = [sheep, cow, llama];
-addFriend(cow, sheep);
-addFriend(llama, cow);
-console.log(myFarm);
+  window.y = 'also, global!'; // all global variables are attached to the window environment
+ }
 
-var addMatchesArray = function(farm) {
-    for (var animal in farm) {
-        farm[animal].matches = [];
-    }
-}
-
-var giveMatches = function(farm) {
-    for (var animal in farm) {
-        farm[animal].matches.push(farm[animal].friends[0]);
-    }
-}
-
-// Nesting Exercise Code Here //
-
-var friendUsernames = [];
-
-var friends = [];
-
-var animals = myFarm;
-friends.push(animals[0].username, animals[1].username);
-
-console.log(friends); // --> Array["Cloudy", "Bessy"]
-
-var relationships = {};
-
-relationships.friends = friends;
-console.log(relationships); // --> Object { friends: Array[2]} --- contains "Cloudy" and "Bessy" in friends Array
-
-console.log(Object.keys(relationships).length); // --> 1
-
-var matches = [];
-
-relationships.matches = matches;
-relationships.matches.push('pig');
-console.log(relationships); // --> Object { friends: Array[2], matches: Array[1] } --> matches array contains "pig" at 0 index
-
-for (var i = 0; i < animals.length; i++) {
-    animals[i].relationships = relationships;
-}
-
-console.log(animals);
-
+ console.log(x); // 'global'
